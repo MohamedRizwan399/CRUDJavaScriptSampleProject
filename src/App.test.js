d5b13efb0a9408f05ed784988694e7ae4ea5aad2
Mohamed Rizwan <mohamedrizwan399@gmail.com>
2024-11-09 21:46:42 +0530

    #TRAPWLS-19 test case updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import Demo from './Demo';
+import Popup from './popup/Popup';
+import { APP_INITIAL_POPUP_MSG } from './utility/constants';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+//clear local storage
+beforeEach(() => {
+    localStorage.clear();
+});
 
-
-// test('demo state test', () => {
-//   render(<Demo />);
-//   expect().toBeInTheDocument();
-// });
+describe('App component', () => {
+    test('renders popup with image, message, and button', () => {
+        const mockSetShowPopup = jest.fn(); //mock function
+    
+        // Render the Popup component
+        render(<Popup setShowPopup={mockSetShowPopup} />);
+    
+        const popupImage = screen.getByRole('img');
+        expect(popupImage).toBeInTheDocument();
+        expect(popupImage).toHaveAttribute('src','favicon1.ico');
+    
+        const message = screen.getByText(APP_INITIAL_POPUP_MSG);
+        expect(message).toBeInTheDocument();
+    
+        //check ok button
+        const button = screen.getByRole('button',{name:/OK/i});
+        expect(button).toBeInTheDocument();
+      });
+})
--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,1 +1,3 @@
+export const APP_NAME = "Javascript CRUD with SampleTasks";
 export const LOGIN_TOAST_INFO = "Right now not implemented, check GoogleLogin";
+export const APP_INITIAL_POPUP_MSG = "Here’s an important message for you!";
