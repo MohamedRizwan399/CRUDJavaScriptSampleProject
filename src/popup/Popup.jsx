c35ebdc39040c26b7e40589da4b84ba119f28dac
Mohamed Rizwan <mohamedrizwan399@gmail.com>
2024-11-09 22:41:07 +0530

    #TRAPWLS-19 Popup shown intially in Login screen

---
+++ src/popup/Popup.jsx
@@ -0,0 +1,21 @@
+import { APP_INITIAL_POPUP_MSG } from "../utility/constants";
+
+
+const Popup = (props) => {
+    const { setClosePopup } = props;
+
+    const closePopup = () => {
+        setClosePopup(true);
+    }
+
+    return(
+        <div className="popup-overlay">
+            <div className="popup">
+                <img src="favicon1.ico" alt='' />
+                <p dangerouslySetInnerHTML={{__html: APP_INITIAL_POPUP_MSG}}></p>
+                <button onClick={closePopup}>OK</button>
+            </div>
+        </div>
+    );
+}
+export default Popup;
