30ae3be754a5d91a4a1a3c897248b40866c50098
Mohamed Rizwan <mohamedrizwan399@gmail.com>
2025-08-05 03:52:37 +0530

    TRAPWLS-19 files removed for production deployment

--- src/utility/regexValidator.jsx
+++
@@ -1,9 +0,0 @@
-export const emailValidator = username => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(username);
-}
-
-export const passwordValidator = password => {
-    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$+%^&*_\-~]).{8,}$/;
-    return passwordRegex.test(password);
-}
