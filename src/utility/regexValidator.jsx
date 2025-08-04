3568dc81f5b1f081453ac75b1bfc0d45e33e94e6
Mohamed Rizwan <mohamedrizwan399@gmail.com>
2025-08-04 22:45:37 +0530

    #TRAPWLS-19 Code optimization, UX enhanced and bug fixes

--- src/utility/regexValidator.jsx
+++ src/utility/regexValidator.jsx
@@ -4,6 +4,6 @@ export const emailValidator = username => {
 }
 
 export const passwordValidator = password => {
-    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$+%^&*_\-~]).{8,}$/;
     return passwordRegex.test(password);
 }
